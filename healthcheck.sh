844d2df263dda61bf388fc14fcbaa5d5855abf7d
Tim Oliver Rabl <rabl@komro.net>
2022-09-09 12:34:20 +0200

    fix SC2086 and SC3014

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,10 +3,10 @@
 set -e 
 
 PORT=$(netstat -lntp | grep python3 | awk '{gsub(".*:","",$4);print $4}')
-RESPONSE=$(curl -fsL -w '%{http_code}' -o /dev/null http://localhost:${PORT:-8001})
+RESPONSE=$(curl -fsL -w '%{http_code}' -o /dev/null "http://localhost:${PORT:-8001}")
 
 if [ "${RESPONSE}x" != "x" ]; then
-    if [ "${RESPONSE}" == "200" ]; then
+    if [ "${RESPONSE}" = "200" ]; then
         return 0
     fi
 fi
